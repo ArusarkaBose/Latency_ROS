e96f4383fdffcd3954eae3e642f147bd415f9745
Arusarka <arusarkabose@gmail.com>
2020-07-26 11:34:10 +0530

    Updated latency client

--- src/latencycheckclient.py
+++ src/latencycheckclient.py
@@ -6,7 +6,8 @@ from codebase.srv import LatencyCheckResponse
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pickle as pl
+import time
+import json
 
 import rospy
 import sys
@@ -21,37 +22,53 @@ def latencycheck_client(time):
         print "Service call failed: %s"%e
 
 if __name__ == "__main__":
-    num_queries=10000
-    latencies=[]
+    num_queries=1000
+    num_bursts=10
+
     rospy.init_node('latency_check_client')
-    for i in range(num_queries):
-        query_time=rospy.get_rostime()
-        return_time = latencycheck_client(query_time)
-        current_time=rospy.get_rostime()
-        latency=current_time-return_time
-        print("Latency= ",latency)
-        latencies.append((latency.secs*1000)+float(latency.nsecs)/(10**6))
 
-    mean=sum(latencies[1:])/len(latencies[1:])
-    standard_dev=sum([((x - mean) ** 2) for x in latencies[1:]]) / len(latencies[1:])    
+    latency_bursts=[]
 
-    print("Mean = ",mean)
-    print("Error Bar = ",mean+standard_dev)
-    print("Max = ",max(latencies[1:]))
-    print("Min = ",min(latencies[1:]))
+    for i in range(num_bursts):
+        latencies=[]
+        for i in range(num_queries):
+            query_time=rospy.get_rostime()
+            return_time = latencycheck_client(query_time)
+            current_time=rospy.get_rostime()
+            latency=current_time-return_time
+            print("Latency= ",latency)
+            latencies.append((latency.secs*1000)+float(latency.nsecs)/(10**6))
+
+        mean=np.mean(latencies)
+        standard_dev=np.std(latencies)    
+
+        print("Mean = ",mean)
+        print("Error Bar = ",mean+standard_dev)
+        print("Max = ",max(latencies))
+        print("Min = ",min(latencies))
+
+        latency_bursts.append(latencies)
+        time.sleep(5)
+
+    y=[np.mean(latencies) for latencies in latency_bursts]
+    x=np.arange(1,len(y)+1,1)
+    yerr=[np.std(latencies) for latencies in latency_bursts]
+
+    latency_ROS1=dict()
+    latency_ROS1["y"]=y
+    latency_ROS1["x"]=x.tolist()
+    latency_ROS1["yerr"]=yerr
+
+    with open('latency_ROS1.json','w+') as json_file:
+        json.dump(latency_ROS1,json_file)
     
 
-    #fig_handle = pl.load(open('/home/arusarka/dev_ws/src/devel_space/devel_space/ros2_latency.pickle','rb'))
-    #x_old = fig_handle.axes[0].lines[0].get_data()[0]
-    #y_old = fig_handle.axes[0].lines[0].get_data()[1]
-
+    fig_handle=plt.figure()
     ax=plt.subplot()
 
-    #ax.plot(x_old,y_old)
-
-    ax.plot(latencies)
-    ax.set(xlabel='Query Number',ylabel='Latency (in ms) ',title='Latency Plot')
-    plt.xticks(np.arange(0, len(latencies)+1, len(latencies)/20))
+    ax.errorbar(x,y,yerr)
+    ax.set(xlabel='Query Number (at 5 sec intervals)',ylabel='Latency (in ms) ',title='Latency Plot')
+    plt.xticks(np.arange(min(x), max(x)+1, 1.0))
     ax.grid()
 
     plt.show()
